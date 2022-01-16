ad46d636043cc2b72bb9fcd3230a5562076971d5
carlos diaz <diazmelian.carlos@gmail.com>
2022-01-16 12:18:14 +0000

    created basic style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,35 @@
-def preprends_motivation(your_message)
-  if your_message[-1] == "?"
-    @answer = "I can feel your motivation! Silly question, get dressed and go to work!"
-  else
-    @answer = "I can feel your motivation! I don't care, get dressed and go to work!"
-  end
-end
+# frozen_string_literal: true
 
-def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
-
-  if your_message.upcase == "I AM GOING TO WORK RIGHT NOW!"
-    @answer = ""
-  elsif your_message == your_message.upcase
-    preprends_motivation(your_message)
-  elsif your_message[-1] == "?"
-    @answer = "Silly question, get dressed and go to work!"
-  else
-    @answer = "I don't care, get dressed and go to work!"
-  end
-end
-
+# question controller
 class QuestionsController < ApplicationController
   def ask
-    question
   end
 
   def answer
     question = params[:question]
     coach_answer_enhanced(question)
+  end
+
+  private
+
+  def preprends_motivation(your_message)
+    if your_message[-1] == '?'
+      'I can feel your motivation! Silly question, get dressed and go to work!'
+    else
+      'I can feel your motivation! I don`t care, get dressed and go to work!'
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    # TODO: return coach answer to your_message, with additional custom rules!
+    @answer = if your_message.upcase == 'I AM GOING TO WORK RIGHT NOW!'
+                ''
+              elsif your_message == your_message.upcase
+                preprends_motivation(your_message)
+              elsif your_message[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don`t care, get dressed and go to work!'
+              end
   end
 end
